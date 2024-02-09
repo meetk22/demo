e3f9d1e6527c3398a9aab0685ea6461903b8e50e
Meet Khanuja <meetkhanuja22@gmail.com>
2024-02-10 00:45:03 +0530

    first commit

---
+++ src/components/Github/Gitbus.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+
+function Gitbus() {
+
+    useEffect(() => {
+        first
+    })
+  return (
+    <div className='text-center m-4 bg-gray-600 text-whitetext-3xl'>Gitbus Followers: </div>
+  )
+}
+
+export default Gitbus
